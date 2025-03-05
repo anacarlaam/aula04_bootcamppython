ac1f515950a0397a4e4902711c3f2ae9007982a3
anacarlaam <anacarlaam@gmail.com>
2025-03-05 19:17:48 -0300

    incluindo dicionarios

---
+++ main.py
@@ -0,0 +1,13 @@
+# Exerc 03- Crie um dicionario para armazenar informacoes de um livro
+# incluindo titulo, autor e ano de publicacao. Imprima sua informacao
+
+from typing import Dict, Any
+
+livro: Dict[str, Any] = {
+    "Titulo": "Game of Thrones",
+    "Autor": "Estagiario",
+    "Ano": 2005
+}
+lista_de_elementos: list
+
+print (livro)
